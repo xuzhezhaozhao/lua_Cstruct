41014926e89338d2c7cc3f9bdb19a752437ea221
xuzhezhao <zhezhaoxu@gmail.com>
2016-09-07 21:50:29 +0800

    finish lua get(), set(), it seems ok

--- mylib.c
+++ mylib.c
@@ -9,19 +9,59 @@
 static int C_alloc(lua_State* L) {
 	lua_Integer n = luaL_checkinteger(L, 1);
 	lua_Integer s = luaL_checkinteger(L, 2);
-	void *data = calloc(n, s);
-	if (data == NULL) {
-		/* error handle */
-		lua_pushnil(L);
-		lua_pushstring(L, strerror(errno));
-		return 2;
-	}
-	lua_pushlightuserdata(L, data);
+	void *p = lua_newuserdata(L, n*s);
+	memset(p, 0, n*s);
 	return 1;
+}
+
+static int get_number(lua_State* L) {
+	char *data = (char *)lua_touserdata(L, 1);
+	luaL_argcheck(L, data != NULL, 1, "Wrong Parameter (expected userdata).");
+	lua_Integer len = luaL_checkinteger(L, 2);
+	lua_Integer nth = luaL_checkinteger(L, 3);
+	lua_Integer offset = luaL_checkinteger(L, 4);
+	double *p = (double *)(data + len*nth + offset);
+	lua_pushnumber(L, (lua_Number)(*p));
+	return 1;
+}
+
+static int set_number(lua_State* L) {
+	char *data = (char *)lua_touserdata(L, 1);
+	luaL_argcheck(L, data != NULL, 1, "Wrong Parameter (expected userdata).");
+	lua_Integer len = luaL_checkinteger(L, 2);
+	lua_Integer nth = luaL_checkinteger(L, 3);
+	lua_Integer offset = luaL_checkinteger(L, 4);
+	lua_Number value = luaL_checknumber(L, 5);
+	double *p = (double *)(data + len*nth + offset);
+	(*p) = (double)value;
+	return 0;
+}
+static int get_string(lua_State* L) {
+	char *data = (char *)lua_touserdata(L, 1);
+	lua_Integer len = luaL_checkinteger(L, 2);
+	lua_Integer nth = luaL_checkinteger(L, 3);
+	lua_Integer offset = luaL_checkinteger(L, 4);
+	const char **p = (const char **)(data + len*nth + offset);
+	lua_pushstring(L, *p);
+	return 1;
+}
+static int set_string(lua_State* L) {
+	char *data = (char *)lua_touserdata(L, 1);
+	lua_Integer len = luaL_checkinteger(L, 2);
+	lua_Integer nth = luaL_checkinteger(L, 3);
+	lua_Integer offset = luaL_checkinteger(L, 4);
+	const char *s = luaL_checkstring(L, 5);
+	const char **p = (const char **)(data + len*nth + offset);
+	(*p) = s;
+	return 0;
 }
 
 static luaL_Reg mylib[] = {
 	{"calloc", C_alloc},
+	{"get_number", get_number},
+	{"set_number", set_number},
+	{"get_string", get_string},
+	{"set_string", set_string},
 	{NULL, NULL}
 };
 
--- test.lua
+++ test.lua
@@ -1,12 +1,11 @@
 require "mylib"
 
 -- char[16], int, int
-student = {name="number", id="string", score="number"}
-school = {name="string", student=student, pub="number", id="number", http="number"}
+local student = {id="number", name="string", score="number"}
+local school = {name="string", id="number",  pub="number", student=student, http="number"}
 
 local numLen = 8		-- store double
 local strLen = 8		-- store a pointer
-
 --[
 -- struct = {
 -- _length_
@@ -23,17 +22,19 @@ local function define_structure(ltype, offset)
 		offset = 0
 	end
 	local struct = {_length_ = 0}
-	for k, v in pairs(rtype) do
+	for k, v in pairs(ltype) do
 		local curoffset = offset + struct._length_
 		if (v == "number") then
-			struct[k] = {type = 0, offset = curoffset}
+			struct[k] = {_type_ = 0, _offset_ = curoffset}
 			struct._length_ = struct._length_ + numLen
 		elseif (v == "string") then
-			struct[k] = {type = 1, offset = curoffset}
+			struct[k] = {_type_ = 1, _offset_ = curoffset}
 			struct._length_ = struct._length_ + strLen
 		elseif (type(v) == "table") then
 			local substruct = define_structure(v, curoffset)
-			struct[k] = {type = 2, offset = curoffset, struct = substruct}
+			struct[k] = substruct;
+			struct[k]._type_ = 2;
+			struct[k]._offset_ = curoffset;
 			struct._length_ = struct._length_ + substruct._length_
 		end
 	end
@@ -49,13 +50,13 @@ function create_structure(ltype, n)
 end
 
 function get(stype, nth, key)
-	if (key.type == 0) then
+	if (key._type_ == 0) then
 		-- number
-		return mylib.get_number(stype._data_, nth, key.offset)
-	elseif (key.type == 1) then
+		return mylib.get_number(stype._data_, stype._length_, nth, key._offset_)
+	elseif (key._type_ == 1) then
 		-- string
-		return mylib.get_string(stype._data_, nth, key.offset)
-	elseif (key.type == 2) then
+		return mylib.get_string(stype._data_, stype._length_, nth, key._offset_)
+	elseif (key._type_ == 2) then
 		-- table, error
 		return nil
 	else
@@ -63,5 +64,15 @@ function get(stype, nth, key)
 	end
 end
 
-function set(stype, nth, key)
+function set(stype, nth, key, value)
+	if (key._type_ == 0) then
+		-- number
+		mylib.set_number(stype._data_, stype._length_, nth, key._offset_, value)
+	elseif (key._type_ == 1) then
+		-- string
+		mylib.set_string(stype._data_, stype._length_,  nth, key._offset_, value)
+	elseif (key._type_ == 2) then
+		-- table, error
+	else
+	end
 end
